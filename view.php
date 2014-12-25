fec568b9bafb4a588145b410a279b036f56df5e9
fatima <fatima.rini786@gmail.com>
2014-12-25 09:11:47 +0600

    create new project from Understanding crud

---
+++ view.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<form action="add.php" method="POST">
+    <label>First Name:</label>
+    <input type="text" name="firstName" />
+    <br>
+    <label>Last Name:</label>
+    <input type="text" name="lastName" />
+    <br>
+    <label>Contact:</label>
+    <input type="text" name="phone" />
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
